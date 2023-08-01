f59a782faffef64c47d2f82515e1096bfd8f8e9f
Salmanhttps <141110063+Salmanhttps@users.noreply.github.com>
2023-08-01 11:01:44 +0530

    Update server.js

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ connectDB();
 
 // esmoudle fix
 // const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+// const __dirname = path.dirname(__filename);
 
 //rest objecct
 const app = express();
